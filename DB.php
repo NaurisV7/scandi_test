83907d87dcc4d2c974aa6c1635173e1bd2d33010
valainis.nauris@gmail.com <73598402+NaurisV7@users.noreply.github.com>
2021-05-18 20:23:39 +0300

    Cant send data to MqSQL

--- DB.php
+++ DB.php
@@ -25,20 +25,15 @@ class DB {
   echo "Connected successfully";
   }
 
+  $sql = "INSERT INTO furniture VALUES (
+      
+  )";
+
+  if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+  } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+  }
+
 }
-
-
-
-$sql = "INSERT INTO MyGuests (firstname, lastname, email)
-VALUES ('John', 'Doe', 'john@example.com')";
-
-if ($conn->query($sql) === TRUE) {
-  echo "New record created successfully";
-} else {
-  echo "Error: " . $sql . "<br>" . $conn->error;
-}
-
-$conn->close();
-?>
-
 
